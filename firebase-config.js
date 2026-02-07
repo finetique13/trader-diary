e1d447214394f91fde97da7a91c5e59dcca2078b
finetique13 <vladisolin13@bk.ru>
2026-02-07 21:47:32 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,3 @@
+// Для синхронизации на всех устройствах добавьте конфиг Firebase.
+// См. firebase-config.example.js и https://console.firebase.google.com
+window.TRADER_DIARY_FIREBASE_CONFIG = null;
